1fb4872c865d20fb4eca4fbdaa7d8aeb7ad12148
xiefeimcu <1120181085@qq.com>
2017-08-16 21:51:01 +0800

    add config

---
+++ HardwareLib/config.c
@@ -0,0 +1,21 @@
+/*
+ * config.c
+ *
+ *  Created on: 2017Äê8ÔÂ16ÈÕ
+ *      Author: xiefei
+ */
+#include "usart.h"
+#include "config.h"
+#include "stm32f1xx_hal.h"
+
+uint8_t load_config(void){
+
+
+}
+
+uint8_t upData_config(){
+
+/*reStartMcu?*/
+}
+
+
---
+++ HardwareLib/config.h
@@ -0,0 +1,49 @@
+/*
+ * config.h
+ *
+ *  Created on: 2017Äê8ÔÂ16ÈÕ
+ *      Author: Administrator
+ */
+
+#ifndef CONFIG_H_
+#define CONFIG_H_
+
+#include "stm32f1xx_hal.h"
+
+typedef struct{
+	uint16_t mcuStopTime;
+	uint16_t lcdCloseTime;
+	uint16_t lcdcloseBkTime;
+}sysConfig_t;
+
+typedef enum{
+	BAUD_2400,
+	BAUD_4800,
+	BAUD_9600,
+	BAUD_14400,
+	BAUD_19200,
+	BAUD_57600,
+	BAUD_115200,
+}comBaudRateType;
+
+typedef struct{
+	comBaudRateType baudRateRs4851;
+	comBaudRateType baudRateRs4852;
+	comBaudRateType baudRateRs2321;
+	comBaudRateType baudRateRs2322;
+}comBaudRate_t;
+
+typedef struct {
+	uint8_t centreStationAddr;
+
+}upDataArg_t;
+
+typedef struct{
+	sysConfig_t    sysConfig;
+	comBaudRate_t  comBaudRate;
+	upDataArg_t    upDataArg;
+}rtuParameter_t;
+
+extern rtuParameter_t rtuParameter;
+
+#endif /* CONFIG_H_ */
